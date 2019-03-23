bc97e13f9ce40afa59205a3dcec29cb0ff345e42
Ben Attenborough <ben.attenborough@sfdsystems.com>
2019-03-23 17:08:07 +0000

    Add plugin boilerplate

---
+++ includes/class-codemirror-editor-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/BenAttenborough
+ * @since      1.0.0
+ *
+ * @package    Codemirror_Editor
+ * @subpackage Codemirror_Editor/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Codemirror_Editor
+ * @subpackage Codemirror_Editor/includes
+ * @author     Ben Attenborough <battenborough@gmail.com>
+ */
+class Codemirror_Editor_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-codemirror-editor-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/BenAttenborough
+ * @since      1.0.0
+ *
+ * @package    Codemirror_Editor
+ * @subpackage Codemirror_Editor/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Codemirror_Editor
+ * @subpackage Codemirror_Editor/includes
+ * @author     Ben Attenborough <battenborough@gmail.com>
+ */
+class Codemirror_Editor_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
